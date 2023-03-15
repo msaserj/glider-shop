945b6ad7d48f4b7d7c98fa6a6e7a8310054e9389
msa <serg.ks@gmail.com>
2023-03-15 22:48:09 +0400

    test commit

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Header } from '../../components/Header/Header';
 import { Gliders } from '../../components/gliders/gliders';
 
 export const HomePage = () => {
   return (
     <React.Fragment>
-      {/*<Header/>*/}
       <Gliders />
     </React.Fragment>
   );
